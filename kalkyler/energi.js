0574700c470a490006e7763eb7ecec7457db1793
eolhx1 <eolhx1@gmail.com>
2026-07-23 17:49:08 +0200

    Last Sync: 2026-07-23 17:49 (Mobile)

---
+++ kalkyler/energi.js
@@ -0,0 +1,7 @@
+// =================================================================
+// ENERGI KALKYLER
+// =================================================================
+
+export const energiKalkyler = [
+    // Här lägger vi till rena energiberäkningar framöver (t.ex. U-värde, effektbehov, graddagar etc.)
+];
